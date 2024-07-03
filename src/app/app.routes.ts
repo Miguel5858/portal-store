78a9dd8f2da730e1e0413e969f05ece8fb343cb9
Miguel5858 <jmti4884@gmail.com>
2024-07-03 13:37:09 -0500

    tabla de productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { ProductsComponent } from './products/products.component'; // Importa el componente de productos
 
 
 
@@ -8,5 +9,6 @@ import { RegisterComponent } from './auth/register/register.component';
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'products', component: ProductsComponent }, // Ruta hacia el componente de productos
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
