fd4a6d560056692043d690e2bd28940f7c7a9cab
Miguel5858 <jmti4884@gmail.com>
2024-07-04 10:34:40 -0500

    CREANDO LA PANTALLA PRINCIPAL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ProductsComponent } from './products/products.component'; // Importa el componente de productos
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
+
 
 
 
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
+    { path: '', component: WelcomeScreenComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'products', component: ProductsComponent }, // Ruta hacia el componente de productos
     { path: '**', redirectTo: '', pathMatch: 'full' }
